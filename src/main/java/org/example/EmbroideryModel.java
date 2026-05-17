66e23f133a903556c247db9b8469f0152d1d218d
angelina bilyk <angelinabilyk133@gmail.com>
2026-05-17 16:34:34 +0300

    add final int

---
+++ src/main/java/org/example/EmbroideryModel.java
@@ -0,0 +1,9 @@
+package org.example;
+import java.awt.Color;
+
+public class EmbroideryModel {
+    private final int rows = 40;
+    private final int cols = 40;
+    private final int cellSize = 16;
+
+}
